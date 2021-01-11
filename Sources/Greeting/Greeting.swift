a49b658f70b630b790001d6cce113438329119f3
Min-ho Kim <xho1995@gmail.com>
2021-01-11 23:01:30 +0900

    Initial Commit

---
+++ Sources/Greeting/Greeting.swift
@@ -0,0 +1,3 @@
+struct Greeting {
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import GreetingTests
+
+var tests = [XCTestCaseEntry]()
+tests += GreetingTests.allTests()
+XCTMain(tests)
